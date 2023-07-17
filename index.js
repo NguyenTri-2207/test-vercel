db9fb4d0228ebc8c72bf4d11f8b4073109a0da4b
TriNguyen <tri.nguyen@gleads.vn>
2023-07-17 22:47:49 +0700

    add

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 
 // morgan
-
+// add view template engine
+app.set("view engine", "pug");
 // Use Express middleware to configure routing
-app.get("/", (req, res) => res.send("home"));
+app.get("/", (req, res) => res.render("home"));
 app.listen(4000, () => {
   console.log(`Server Started at ${4000}`);
 });
